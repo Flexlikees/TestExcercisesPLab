23cee796b920dc6734f6d0f7208cecafb97183fd
YGene <zheneks8@mail.ru>
2024-08-24 11:18:05 +0300

    task1 full completed

---
+++ task1/task1 (old wo args).cs
@@ -0,0 +1,45 @@
+﻿/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Задайте круговой массив от 1 до n. \nВведите n (n > 0): ");
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.WriteLine("\nОшибка: неверный формат");
+            Console.Write("\nВведите n (n > 0): ");
+        }
+
+        int[] mas = new int[n];
+        Console.Write($"\nПолученный массив: ");
+        for (int i = 0; i < n; i++)
+        {
+            mas[i] = i + 1;
+            Console.Write(mas[i]);
+        }
+
+        Console.Write("\n\nЗадайте интервал движения по массиву - m. \nВведите m (m > 0, m < n): ");
+        int m;
+        while (!int.TryParse(Console.ReadLine(), out m) || m <= 0 || m > n)
+        {
+            Console.WriteLine("\nОшибка - неверный формат");
+            Console.Write("\nУкажите m (m > 0, m < n): ");
+        }
+
+        string path = "";
+        int idx = 0;
+
+        do
+        {
+            path += mas[idx];
+            idx = (idx + m - 1) % n;
+        } 
+        while (idx != 0);
+
+        Console.WriteLine("Результирующий путь: " + path);
+    }
+}
+*/
--- task1/task1.cs
+++ task1/task1.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Ошибка: задайте все необходимые аргументы командной строки (n, m)\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("FAQ: элементы массива от 1 до n; m - интервал движения по массиву");
+            Console.ForegroundColor = ConsoleColor.White;
+            return;
+        }
+
+        if (!int.TryParse(args[0], out int n) || !int.TryParse(args[1], out int m) || n < 1 || m < 1 || m > n)
+        {
+            Console.WriteLine("Ошибка: неверный формат аргументов командной строки \n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("FAQ: n > 0; m > 0; m <= n");
+            Console.ForegroundColor = ConsoleColor.White;
+            return;
+        }
+
+        Console.WriteLine($"Заданные аргументы: n = {n}, m = {m}\n");
+        int[] mas = new int[n];
+        Console.Write($"Круговой массив: ");
+        for (int i = 0; i < n; i++)
+        {
+            mas[i] = i + 1;
+            Console.Write(mas[i]);
+        }
+
+        string path = "";
+        int idx = 0;
+
+        do
+        {
+            path += mas[idx];
+            idx = (idx + m - 1) % n;
+        }
+        while (idx != 0);
+
+        Console.WriteLine("\n\nПолученный путь: " + path);
+    }
+}
